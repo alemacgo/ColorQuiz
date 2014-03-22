c54454f272767ab92d86eb11f99221af0e7659e5
Alejandro Machado <alemacgo@gmail.com>
2014-03-22 17:04:33 +0000

    Initial Commit

---
+++ ColorQuiz/AMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AMAppDelegate.h
+//  ColorQuiz
+//
+//  Created by Alejandro Machado on 14/3/22.
+//  Copyright (c) 2014 M-ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorQuiz/AMViewController.h
@@ -0,0 +1,13 @@
+//
+//  AMViewController.h
+//  ColorQuiz
+//
+//  Created by Alejandro Machado on 14/3/22.
+//  Copyright (c) 2014 M-ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMViewController : UIViewController
+
+@end
